c0a39e9e1cd1c36555ece5319e57d478a8f5a717
GitHubik0 <vik9vik9vik@gmail.com>
2023-12-09 17:57:38 +0200

    cat life

--- main.py
+++ main.py
@@ -1,66 +1,106 @@
 import random
-class Life:
+class Cat_life:
     def __init__(self, name):
         self.name = name
         self.gladness = 100
-        self.progress = 0
-        self.eat = 20
+        self.progress = 5
+        self.eat = 30
+        self.energy = 25
         self.alive = True
 
     def to_study(self):
         print('Час для навчання команд')
-        self.progress += 10
+        self.progress += 15
         self.gladness -= 5
-        self.eat -= 5
+        self.eat -= 10
+        self.energy -= 30
 
     def to_sleep(self):
-        print('Це час для сну')
-        self.gladness += 5
-        self.eat -= 10
+        print('Час для сну')
+        self.gladness += 10
+        self.eat -= 20
+        self.energy += 60
 
     def to_chill(self):
-        print('Це час для відпочинку')
+        print('Час для відпочинку')
         self.progress -= 5
-        self.gladness += 10
+        self.gladness += 15
         self.eat -= 5
+        self.energy -= 5
 
     def to_eat(self):
-        print('Це час для їжі')
-        self.progress += 5
+        print('Час для їжі')
         self.gladness += 10
-        self.eat += 30
+        self.eat += 60
+        self.energy += 15
+
+    def to_go_walk(self):
+        print('Час для прогулянки')
+        self.gladness += 15
+        self.eat += -25
+        self.energy += -15
 
     def is_alive(self):
-        if self.progress < -2:
+        if self.progress <= -15:
             print('Йди розвивайся')
             self.alive = False
 
-        elif self.gladness <= 0:
-            print('Упс в тебе депресія')
+        elif self.gladness <= -10:
+            print('Упс, в тебе депресія')
             self.alive = False
 
-        elif self.progress > 5:
-            print('Ти молодець')
+        elif self.progress >= 500:
+            print('Молодець, ти став найрозумнішим котом світу!')
+            self.alive = False
+
+        elif self.eat <= -15:
+            print('Ти зголоднів')
+            self.alive = False
+
+        elif self.eat >= 300:
+            print('Ти став жирним. Тебе здали в притулок >:(')
+            self.alive = False
+
+        elif self.energy <= -15:
+            print('У тебе не було енергії щоб гратися. Твої господарі здали тебе в притулок!')
+            self.alive = False
+
+        elif self.energy >= 200:
+            print('У тебе було забагато енерії. Твої господарі здали тебе в притулок!')
             self.alive = False
 
     def end_of_the_day(self):
-        print(f"щастя - {self.gladness}")
-        print(f"{round(self.progress,2)}")
+        print(f"Щастя - {self.gladness}")
+        print(f"Розум - {round(self.progress,2)}")
+        print(f"Їжа - {round(self.eat, 3)}")
+        print(f"Енергія - {round(self.energy, 4)}")
 
     def live(self, day):
-        day = 'Day' + str(day) + 'of' + self.name + 'life'
+        day = 'Day ' + str(day) + ' of ' + self.name + ' life'
         print(f"{day:^50}")
-        kubik = random.randint(1, 3)
+        kubik = random.randint(1, 5)
         if kubik == 1:
             self.to_study()
+            self.end_of_the_day()
+            self.is_alive()
         elif kubik == 2:
             self.to_sleep()
+            self.end_of_the_day()
+            self.is_alive()
         elif kubik == 3:
             self.to_chill()
             self.end_of_the_day()
             self.is_alive()
+        elif kubik == 4:
+            self.to_eat()
+            self.end_of_the_day()
+            self.is_alive()
+        elif kubik == 5:
+            self.to_go_walk()
+            self.end_of_the_day()
+            self.is_alive()
 
-nick = Life(name='Patrik')
+nick =Cat_life(name='Patrik')
 for day in range(365):
     if nick.alive == False:
         break
